1e5dfb77f2ff27a01fa28eb307403a38dc5bf294
Navaneeth.P <navaneethpalakkal285@gmail.com>
2024-11-20 18:24:57 +0530

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class details extends StatefulWidget {
+  const details({super.key});
+
+  @override
+  State<details> createState() => _detailsState();
+}
+
+class _detailsState extends State<details> {
+  @override
+  Widget build(BuildContext context) {
+     int _currentIndex = 0;
+    
+    return Scaffold(backgroundColor: Colors.orange,body:  
+       Padding(
+         padding:  EdgeInsets.only(top: 747,),
+         child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+               ),
+       ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {return Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Profile Screen'),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings Screen'),
+    );
+  }
+} 
+
+    
+    
+  
